b3e8029864bdd6b48f695c0c95e4a7cde7bc064d
dvrakry <dv.rakry@gmail.com>
2022-06-22 12:56:49 +0900

    2022-06-22

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,43 +1,91 @@
-import { Button, Form, Icon, Input, Typography } from "antd";
-import TextArea from "antd/lib/input/TextArea";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Typography, Button, Form, message, Input, Icon } from "antd";
 import Dropzone from "react-dropzone";
 import axios from "axios";
+import { useSelector } from "react-redux";
 
 const { Title } = Typography;
+const { TextArea } = Input;
 
-const PrivateOption = [
+const Private = [
   { value: 0, label: "Private" },
   { value: 1, label: "Public" },
 ];
 
-const CategoryOption = [
+const Catogory = [
   { value: 0, label: "Film & Animation" },
-  { value: 1, label: "Autos & Vehicles" },
-  { value: 2, label: "Music" },
-  { value: 3, label: "Pets & Animals" },
+  { value: 0, label: "Autos & Vehicles" },
+  { value: 0, label: "Music" },
+  { value: 0, label: "Pets & Animals" },
+  { value: 0, label: "Sports" },
 ];
 
-function VideoUploadPage() {
-  const [VideoTitle, setVideoTitle] = useState("");
-  const [Description, setDiscription] = useState("");
-  const [Private, setPrivate] = useState(0);
-  const [Category, setCategory] = useState("Firm and Animation");
+function VideoUploadPage(props) {
+  const user = useSelector((state) => state.user);
 
-  const onTitleChange = (e) => {
-    setVideoTitle(e.currentTarget.value);
+  const [title, setTitle] = useState("");
+  const [Description, setDescription] = useState("");
+  const [privacy, setPrivacy] = useState(0);
+  const [Categories, setCategories] = useState("Film & Animation");
+  const [FilePath, setFilePath] = useState("");
+  const [Duration, setDuration] = useState("");
+  const [Thumbnail, setThumbnail] = useState("");
+
+  const handleChangeTitle = (event) => {
+    setTitle(event.currentTarget.value);
   };
 
-  const onDiscriptionChange = (e) => {
-    setDiscription(e.currentTarget.value);
+  const handleChangeDecsription = (event) => {
+    console.log(event.currentTarget.value);
+
+    setDescription(event.currentTarget.value);
   };
 
-  const onPrivateChange = (e) => {
-    setPrivate(e.currentTarget.value);
+  const handleChangeOne = (event) => {
+    setPrivacy(event.currentTarget.value);
   };
 
-  const onCategoryChange = (e) => {
-    setCategory(e.currentTarget.value);
+  const handleChangeTwo = (event) => {
+    setCategories(event.currentTarget.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    if (user.userData && !user.userData.isAuth) {
+      return alert("Please Log in First");
+    }
+
+    if (
+      title === "" ||
+      Description === "" ||
+      Categories === "" ||
+      FilePath === "" ||
+      Duration === "" ||
+      Thumbnail === ""
+    ) {
+      return alert("Please first fill all the fields");
+    }
+
+    const variables = {
+      writer: user.userData._id,
+      title: title,
+      description: Description,
+      privacy: privacy,
+      filePath: FilePath,
+      category: Categories,
+      duration: Duration,
+      thumbnail: Thumbnail,
+    };
+
+    axios.post("/api/video/uploadVideo", variables).then((response) => {
+      if (response.data.success) {
+        alert("video Uploaded Successfully");
+        props.history.push("/");
+      } else {
+        alert("Failed to upload video");
+      }
+    });
   };
 
   const onDrop = (files) => {
@@ -45,31 +93,42 @@ function VideoUploadPage() {
     const config = {
       header: { "content-type": "multipart/form-data" },
     };
+    console.log(files);
     formData.append("file", files[0]);
 
     axios.post("/api/video/upload", formData, config).then((response) => {
       if (response.data.success) {
-        console.log(response.data);
+        let variable = {
+          filePath: response.data.filePath,
+          fileName: response.data.fileName,
+        };
+        setFilePath(response.data.filePath);
+
+        //gerenate thumbnail with this filepath !
+
+        axios.post("/api/video/thumbnail", variable).then((response) => {
+          if (response.data.success) {
+            setDuration(response.data.fileDuration);
+            setThumbnail(response.data.thumbsFilePath);
+          } else {
+            alert("Failed to make the thumbnails");
+          }
+        });
       } else {
-        console.log("실패숙이");
-        alert("비디오 업로드 실패");
+        alert("failed to save the video in server");
       }
     });
   };
 
-  const onSubmit = (event) => {};
-
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
-        <Title level={2}>Upload Video</Title>
+        <Title level={2}> Upload Video</Title>
       </div>
 
       <Form onSubmit={onSubmit}>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
-          {/* Drop Zone */}
-
-          <Dropzone onDrop={onDrop} multiple={false} maxSize={1000000000}>
+          <Dropzone onDrop={onDrop} multiple={false} maxSize={800000000}>
             {({ getRootProps, getInputProps }) => (
               <div
                 style={{
@@ -88,48 +147,47 @@ function VideoUploadPage() {
             )}
           </Dropzone>
 
-          {/* Thumb Zone */}
-          <div>
-            <img src="" alt="" />
-          </div>
+          {Thumbnail !== "" && (
+            <div>
+              <img src={`http://localhost:5000/${Thumbnail}`} alt="haha" />
+            </div>
+          )}
         </div>
 
         <br />
         <br />
-        <label>Title.</label>
-        <Input onChange={onTitleChange} value={VideoTitle} />
+        <label>Title</label>
+        <Input onChange={handleChangeTitle} value={title} />
         <br />
         <br />
-        <label>Description.</label>
-        <TextArea onChange={onDiscriptionChange} value={Description} />
+        <label>Description</label>
+        <TextArea onChange={handleChangeDecsription} value={Description} />
         <br />
         <br />
 
-        <select onChange={onPrivateChange}>
-          {PrivateOption.map((item, i) => (
-            <option key={i} value={item.value}>
+        <select onChange={handleChangeOne}>
+          {Private.map((item, index) => (
+            <option key={index} value={item.value}>
               {item.label}
             </option>
           ))}
         </select>
-
         <br />
         <br />
 
-        <select onChange={onCategoryChange}>
-          {CategoryOption.map((item, i) => (
-            <option key={i} value={item.value}>
+        <select onChange={handleChangeTwo}>
+          {Catogory.map((item, index) => (
+            <option key={index} value={item.label}>
               {item.label}
             </option>
           ))}
         </select>
-
         <br />
         <br />
 
-        {/* <Button type="submit" size="large" onClick>
+        <Button type="primary" size="large" onClick={onSubmit}>
           Submit
-        </Button> */}
+        </Button>
       </Form>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -41,6 +41,7 @@ app.use("/api/video", require("./routes/video"));
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use("/api/video", express.static("./routes/video"));
+app.use("/uploads", express.static("uploads"));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
